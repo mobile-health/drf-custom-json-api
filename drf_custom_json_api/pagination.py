3062832f3d4757a829b969a32c94a661af55798d
David Dang <quyettien.ictu@gmail.com>
2018-01-03 10:55:41 +0700

    Revert PageNumberPagination

--- drf_custom_json_api/pagination.py
+++ drf_custom_json_api/pagination.py
@@ -1,7 +1,7 @@
 from rest_framework_json_api.pagination import *
 
 
-class PageNumberPagination(PageNumberPagination):
+class CustomPageNumberPagination(PageNumberPagination):
 
 
     def get_paginated_response(self, data):
@@ -28,3 +28,31 @@ class PageNumberPagination(PageNumberPagination):
 
 
 
+class PageNumberPagination(PageNumberPagination):
+
+
+    def get_paginated_response(self, data):
+        next = None
+        previous = None
+
+        if self.page.has_next():
+            next = self.page.next_page_number()
+        if self.page.has_previous():
+            previous = self.page.previous_page_number()
+
+        return Response({
+            'results': data,
+            'meta': {
+                'pagination': OrderedDict([
+                    ('page', self.page.number),
+                    ('pages', self.page.paginator.num_pages),
+                    ('count', self.page.paginator.count),
+                ]),
+                'links': OrderedDict([
+                    ('first', self.build_link(1)),
+                    ('last', self.build_link(self.page.paginator.num_pages)),
+                    ('next', self.build_link(next)),
+                    ('prev', self.build_link(previous))
+                ])
+            }
+        })
