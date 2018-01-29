5266df3bceba27d059961a0e14bc6b471574b58d
Trung <trung.q.nguyen@vntrip.vn>
2018-01-29 17:43:07 +0700

    Update serializer

--- drf_custom_json_api/serializers.py
+++ drf_custom_json_api/serializers.py
@@ -16,28 +16,41 @@ class NotUpdateSerializerMixin(object):
 
 class ResourceIdentifierSerializer(object):
     def __init__(self, *args, **kwargs):
-        self.Meta.is_nested = kwargs.pop("is_nested", False)
         super(ResourceIdentifierSerializer, self).__init__(*args, **kwargs)
 
     def to_representation(self, instance):
-        represent_data = super(ResourceIdentifierSerializer, self).to_representation(instance)
-
-        
-        if not is_response_format_v2(self.context['request']):
+        if 'request' in self.context and not is_response_format_v2(self.context['request']):
+            represent_data = super(ResourceIdentifierSerializer, self).to_representation(instance)
             represent_data['object'] = custom_get_resource_type_from_instance(instance)
             return represent_data
+
+        ret = OrderedDict()
+        fields = self._readable_fields
+        for field in fields:
+            try:
+                attribute = field.get_attribute(instance)
+            except SkipField:
+                continue
+
+            if attribute is None:
+                ret[field.field_name] = None
+            else:
+                field_data = field.to_representation(attribute)
+                if hasattr(self.Meta, 'nested_fields') and field.field_name in self.Meta.nested_fields:
+                    field_data = {
+                        "data": field_data,
+                        "meta": {
+                            "include": [],
+                            "custom": []
+                        }
+                    }
+                ret[field.field_name] = field_data
+        represent_data = ret
 
         data = {
             'object': custom_get_resource_type_from_instance(instance)
         }
         for item in represent_data.items():
             data[item[0]] = item[1]
-        if self.Meta.is_nested:
-            data = {
-                "data": data,
-                "meta": {
-                    "include": [],
-                    "custom": []
-                }
-            }
+
         return data
