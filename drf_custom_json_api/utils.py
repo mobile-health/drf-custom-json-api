5edea2107e28c13f415e8dacbcf2d28796342f53
Trung <trung.q.nguyen@vntrip.vn>
2018-01-30 11:22:18 +0700

    Update default response format setting

--- drf_custom_json_api/utils.py
+++ drf_custom_json_api/utils.py
@@ -1,5 +1,11 @@
 from rest_framework_json_api.utils import *
+from django.conf import settings
 
+
+RESPONSE_VERSION_1 = 'v1'
+RESPONSE_VERSION_2 = 'v2'
+
+AVAILABLE_VERSIONS = [RESPONSE_VERSION_1, RESPONSE_VERSION_2]
 
 def custom_get_resource_type_from_instance(instance):
     #check if have custom field for mongo model
@@ -9,13 +15,21 @@ def custom_get_resource_type_from_instance(instance):
         return get_resource_type_from_instance(instance)
 
 
+def get_default_version():
+    version = RESPONSE_VERSION_1
+    if hasattr(settings, "RESPONSE_FORMAT_VERSION"):
+        version = settings.RESPONSE_FORMAT_VERSION if settings.RESPONSE_FORMAT_VERSION in AVAILABLE_VERSIONS else RESPONSE_VERSION_1
+    return version
 
 def get_response_format_from_request(request):
-	return request.query_params.get('response-format', '')
+    version = str(request.query_params.get('response-format', '')).lower()
+    if version not in AVAILABLE_VERSIONS:
+        version = get_default_version()
+    return version
 
 
 def is_response_format_v2(request):
-	version = get_response_format_from_request(request)
-	if str(version).lower() == 'v2':
-		return True
-	return False
+    version = get_response_format_from_request(request)
+    if version == RESPONSE_VERSION_2:
+        return True
+    return False
