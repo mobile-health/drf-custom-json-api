a3ac55f9cf9db928f18bc4bd97b600bff1b3e89d
David Dang <quyettien.ictu@gmail.com>
2018-02-09 15:49:21 +0700

    Update pagination.py

--- drf_custom_json_api/pagination.py
+++ drf_custom_json_api/pagination.py
@@ -1,8 +1,8 @@
 from rest_framework_json_api.pagination import *
+from django.core.paginator import InvalidPage
 
 
 class CustomPageNumberPagination(PageNumberPagination):
-
 
     def get_paginated_response(self, data):
         next = None
@@ -26,10 +26,36 @@ class CustomPageNumberPagination(PageNumberPagination):
             ])
         })
 
+    def paginate_queryset(self, queryset, request, view=None):
+        """
+        Paginate a queryset if required, either returning a
+        page object, or `None` if pagination is not configured for this view.
+        """
+        page_size = self.get_page_size(request)
+        if not page_size:
+            return None
+
+        paginator = self.django_paginator_class(queryset, page_size)
+        page_number = request.query_params.get(self.page_query_param, 1)
+        if page_number in self.last_page_strings:
+            page_number = paginator.num_pages
+
+        try:
+            self.page = paginator.page(page_number)
+        except InvalidPage as exc:
+            self.request = request
+            self.page = paginator.page(paginator.num_pages)
+            return list()
+
+        if paginator.num_pages > 1 and self.template is not None:
+            # The browsable API should display pagination controls.
+            self.display_page_controls = True
+
+        self.request = request
+        return list(self.page)
 
 
 class PageNumberPagination(PageNumberPagination):
-
 
     def get_paginated_response(self, data):
         next = None
